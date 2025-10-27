55982d5421947557ce852c51fe37cc4925f13b3c
Hari Prasad <hariprasadxx121@gmail.com>
2025-10-27 21:48:23 +0530

    Done palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (n) {
+    let word = n.toLowerCase().replace(/[^a-z0-9]/gi, '')
+    let reverse= word.split('').reverse().join('');
+    return reverse === word;
+    // if (reverse == word){
+    //     //console.log("palindrome")
+    //     return true
+    // }
+    // return false
 };
 
 // Do not edit below this line
