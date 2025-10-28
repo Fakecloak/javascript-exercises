f75197bd77927fe44008887e2d2ac40e7bef660e
Hari Prasad <hariprasadxx121@gmail.com>
2025-10-29 00:11:12 +0530

    done 14th

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if (num<0) return 'ERROR';
+    let x='';
+    for (i=0; i<num;i++){
+        x += str;
+    }
+    return x;
 };
 
+repeatString('hello',10)
 // Do not edit below this line
 module.exports = repeatString;
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
 
+const fibonacci = function(n) {
+    let start = 0;
+    let end =1;
+    let result = [];
+    let temp;
+    while(start<n){
+        result.push(start);
+        temp = start;
+        start = end;
+        end = temp + end;
+    }
+    return result;
 };
+
+//console.log(fibonacci(5))
 
 // Do not edit below this line
 module.exports = fibonacci;
