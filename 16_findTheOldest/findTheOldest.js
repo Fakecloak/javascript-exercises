4d6a0ed0e436971f386c5e6e4198645e70d0c10f
Hari Prasad <hariprasadxx121@gmail.com>
2025-10-30 13:43:13 +0530

    finished all excercise with simple logic

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currYear =new Date().getFullYear() 
 
+    return people.sort((a,b) => {
+        const ageB = (b.yearOfDeath || currYear) - b.yearOfBirth
+        const ageA = (a.yearOfDeath || currYear) - a.yearOfBirth 
+        return ageB-ageA;
+    })[0];
+    
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
